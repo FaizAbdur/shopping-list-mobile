034ce0fdc7eeb186ceeebe2876b81c96431bd6d0
FaizAbdur <faizsocool@gmail.com>
2023-11-22 14:11:01 +0700

    toturial pra 1

--- lib/menu.dart
+++ lib/menu.dart
@@ -17,10 +17,9 @@ class MyHomePage extends StatelessWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
-
   @override
-  Widget build(BuildContext context){
-  return Scaffold(
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: const Text(
           'Shopping List',
@@ -66,53 +65,55 @@ class MyHomePage extends StatelessWidget {
     );
   }
 }
-    class ShopItem {
-    final String name;
-    final IconData icon;
 
-    ShopItem(this.name, this.icon);
-  }
-  class ShopCard extends StatelessWidget {
-    final ShopItem item;
+class ShopItem {
+  final String name;
+  final IconData icon;
 
-    const ShopCard(this.item, {super.key}); // Constructor
+  ShopItem(this.name, this.icon);
+}
 
-    @override
-    Widget build(BuildContext context) {
-      return Material(
-        color: Colors.indigo,
-        child: InkWell(
-          // Area responsive terhadap sentuhan
-          onTap: () {
-            // Memunculkan SnackBar ketika diklik
-            ScaffoldMessenger.of(context)
-              ..hideCurrentSnackBar()
-              ..showSnackBar(SnackBar(
-                  content: Text("Kamu telah menekan tombol ${item.name}!")));
-          },
-          child: Container(
-            // Container untuk menyimpan Icon dan Text
-            padding: const EdgeInsets.all(8),
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    item.icon,
-                    color: Colors.white,
-                    size: 30.0,
-                  ),
-                  const Padding(padding: EdgeInsets.all(3)),
-                  Text(
-                    item.name,
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(color: Colors.white),
-                  ),
-                ],
-              ),
+class ShopCard extends StatelessWidget {
+  final ShopItem item;
+
+  const ShopCard(this.item, {super.key}); // Constructor
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.indigo,
+      child: InkWell(
+        // Area responsive terhadap sentuhan
+        onTap: () {
+          // Memunculkan SnackBar ketika diklik
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(SnackBar(
+                content: Text("Kamu telah menekan tombol ${item.name}!")));
+        },
+        child: Container(
+          // Container untuk menyimpan Icon dan Text
+          padding: const EdgeInsets.all(8),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  item.icon,
+                  color: Colors.white,
+                  size: 30.0,
+                ),
+                const Padding(padding: EdgeInsets.all(3)),
+                Text(
+                  item.name,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ],
             ),
           ),
         ),
-      );
-    }
+      ),
+    );
   }
+}
